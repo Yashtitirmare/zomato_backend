8b3e034dff630b9931de62921292e6efd3a6d5c1
Yash112002 <yash966591@gmail.com>
2022-08-25 15:03:42 +0530

    update file

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use("/pay",paymentRoutes)
 
 //heroku configurations:
 if(process.env.NODE_ENV="production"){
-  app.use(express.static("frontend/build"));
+  app.use(express.static("frontend"));
   // const path = require("path");
   // app.get("*",(req,res)=>{
   //   res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
